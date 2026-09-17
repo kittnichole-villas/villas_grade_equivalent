d2ee7a862abecc965730e55484ee57c862fc34db
kittnichole-villas <kittnicholevillas@gmail.com>
2026-09-17 16:36:21 +0800

    grade_equivalent

--- grade_equivalent.c
+++ grade_equivalent.c
@@ -1,6 +1,32 @@
+#include <stdio.h>
+
 int main() {
 	
-	printf("grade");
+	//declare variables
+	int score;
+	char grade;
+
+	//output: ask the user for their score
+	printf("Enter your score: ");
+
+	//input: score
+	scanf("%d", &score);
+
+	//process: assign the equivalent grade for the score
+	if (score >= 90) {
+		grade = 'A';
+	} else if (score >= 80) {
+		grade = 'B';
+	} else if (score >= 70) {
+		grade = 'C';
+	} else if (score >= 60) {
+		grade = 'D';
+	} else {
+		grade = 'F';
+	}
+
+	//output: grade
+	printf("%c", grade);
 	
 	return 0;
 	
